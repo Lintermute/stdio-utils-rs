db07344ee9bc511a306f4e40726e7d15fcc63c99
Andreas Waidler <waidler@programmers.at>
2020-05-17 13:09:14 +0200

    Consolidate error/result handling

--- src/lib.rs
+++ src/lib.rs
@@ -1,25 +1,68 @@
-type Number = isize;
+use std::error;
+use std::fmt;
+use std::io;
+use std::num;
 
-#[derive(Debug, PartialEq)]
-pub enum Error {
-    ReadingError,
-    ParsingError,
+#[derive(Debug)]
+pub struct ParsingError {
+    input: String,
+    error: num::ParseIntError,
 }
 
-pub fn sum<T>(lines: T) -> Result<Number, Error>
+impl fmt::Display for ParsingError {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f,
+            "Could not parse \"{}\" to number: {:?}",
+            self.input, self.error)
+    }
+}
+
+impl error::Error for ParsingError {}
+
+#[derive(Debug)]
+pub enum ApplicationError {
+    InputError(io::Error),
+    ParsingError(ParsingError),
+}
+
+impl From<io::Error> for ApplicationError {
+    fn from(e : io::Error) -> ApplicationError {
+        ApplicationError::InputError(e)
+    }
+}
+
+impl From<ParsingError> for ApplicationError {
+    fn from(e : ParsingError) -> ApplicationError {
+        ApplicationError::ParsingError(e)
+    }
+}
+
+type Number = isize;
+
+fn as_number(line: &str) -> Result<Number, ParsingError> {
+    // We cannot use From here because ParseIntError
+    // does not contain a reference to offending input.
+    line.trim().parse().map_err(|err|
+         ParsingError {
+             input: String::from(line),
+             error: err
+         })
+}
+
+pub fn sum<T>(lines: T) -> Result<Number, ApplicationError>
 where
-    T: Iterator<Item = String>,
+    T: Iterator<Item = Result<String, io::Error>>,
 {
     lines
-        .map(|s| as_number(&s))
+        .map(|line| Ok(as_number(&line?)?))
         .sum()
 }
 
-pub fn as_number(line: &str) -> Result<Number, Error> {
-    match line.trim().parse() {
-        Ok(num) => Ok(num),
-        Err(_) => Err(Error::ParsingError),
-    }
+pub fn sum_strings<T>(strings: T) -> Result<Number, ApplicationError>
+where
+    T: Iterator<Item = String>,
+{
+    sum(strings.map(|s| Ok(s)))
 }
 
 #[cfg(test)]
@@ -28,41 +71,72 @@ mod tests {
 
     #[test]
     fn parses_a_number() {
-        assert_eq!(Ok(42), as_number("42"));
+        assert_eq!(as_number("42").unwrap(), 42);
     }
 
     #[test]
     fn parses_a_number_with_whitespace() {
-        assert_eq!(Ok(42), as_number("\t42 \n"));
+        assert_eq!(as_number("\t 42\n").unwrap(), 42);
+    }
+
+    #[test]
+    fn fails_on_invalid_character() {
+        let result = as_number(bad_input_char());
+        let msg = result.unwrap_err().to_string();
+        assert!(
+            msg.contains(bad_input_char()),
+            "Offending character not part of error message \"{}\"", msg);
     }
 
     #[test]
     fn fails_on_empty_input() {
-        match as_number("") {
-            Ok(v) => panic!("Empty string was parsed to value {}", v),
-            Err(e) => println!("Got an expected error: {:?}", e),
-        }
+        let msg = as_number("").unwrap_err().to_string();
+        assert!(
+            !msg.contains(bad_input_char()),
+            "Unexpected (hardcoded?) text in error message \"{}\"", msg);
     }
 
     #[test]
     fn empty_stream_returns_zero() {
-        assert_eq!(Ok(0), sum(std::iter::empty()));
+        let stream = std::iter::empty();
+        assert_eq!(sum(stream).unwrap(), 0);
     }
 
     #[test]
-    fn single_element_is_its_sum() {
-        let v = vec!["42".to_string()];
+    fn single_element_is_equal_to_sum() {
+        let stream = vec!["42".to_string()].into_iter();
 
-        assert_eq!(Ok(42), sum(v.into_iter()));
+        assert_eq!(sum_strings(stream).unwrap(), 42);
     }
 
     #[test]
     fn sums_two_elements() {
-        let v = vec![
-            "20".to_string(),
-            "22".to_string()
-        ];
+        let stream = vec![
+            "39".to_string(),
+            "30".to_string()
+        ].into_iter();
 
-        assert_eq!(Ok(42), sum(v.into_iter()));
+        assert_eq!(sum_strings(stream).unwrap(), 69);
     }
+
+    #[test]
+    fn propagates_internal_errors() {
+        let stream = vec!["".to_string()].into_iter();
+        sum_strings(stream).unwrap_err();
+    }
+
+    #[test]
+    fn propagates_external_errors() {
+        let stream = vec![Err(create_io_error())].into_iter();
+        sum(stream).unwrap_err();
+    }
+
+    fn bad_input_char() -> &'static str {
+        "$"
+    }
+
+    fn create_io_error() -> io::Error {
+        io::Error::new(io::ErrorKind::Other, "Mock Error")
+    }
+
 }
--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,19 @@
-use std::error::Error;
-use std::io;
 use std::io::BufRead;
-use std::process;
+use stdio_utils::ApplicationError;
 
 fn main() {
-    let stdin = io::stdin();
+    let stdin  = std::io::stdin();
+    let result = stdio_utils::sum(stdin.lock().lines());
 
-    let stdin = stdin.lock().lines().map(|line| {
-        line.unwrap_or_else(|err| {
-            let cause = err.description();
-            eprintln!("I/O error reading from stdin: {}", cause);
-            process::exit(1);
-        })
-    });
-
-    let sum = stdio_utils::sum(stdin).unwrap_or_else(|err| {
-        eprintln!("Error during operation: {:?}", err);
-        process::exit(1);
+    let sum = result.unwrap_or_else(|err| match err {
+        ApplicationError::InputError(e) => {
+            eprintln!("I/O error reading from stdin: {}", e);
+            std::process::exit(1)
+        }
+        ApplicationError::ParsingError(e) => {
+            eprintln!("Bad input data: {}", e);
+            std::process::exit(1)
+        },
     });
 
     println!("{}", sum);
